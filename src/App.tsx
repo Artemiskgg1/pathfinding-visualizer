a3fa1f0fb2ea24bc0953e929ec05beef5ce09bbf
iynes <abhaghildiyal@gmail.com>
2024-09-14 00:04:12 +0530

    feat: Add Vite environment declaration file
    Add vite-env.d.ts file to the src directory to declare the reference types for Vite client.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
+        Hemlo bro
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/context/PathfindingContext.ts
@@ -0,0 +1,26 @@
+import { createContext, useState } from "react";
+import { AlgorithmType, GridType, MazeType } from "../utils/types";
+
+interface PathfindingContextInterface {
+  algorithm: AlgorithmType;
+  setAlgorithm: (algorithm: AlgorithmType) => void;
+  maze: MazeType;
+  setMaze: (maze: MazeType) => void;
+  grid: GridType;
+  setGrid: (grid: GridType) => void;
+  setIsGraphVisualized: (isGraphVisualized: boolean) => void;
+}
+
+export const PathfindingContext = createContext<
+  PathfindingContextInterface | undefined
+>(undefined);
+
+export const PathfindingProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [algorithm, setAlgorithm] = useState<AlgorithmType>("BFS");
+  const [maze, setMaze] = useState<MazeType>("NONE");
+  const [grid, setGrid] = useState<GridType>([]);
+};
