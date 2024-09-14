f1c6cb7ddcb40198da60db3b4d0b36ef3f1d12e2
iynes <abhaghildiyal@gmail.com>
2024-09-14 20:59:14 +0530

    refactor: Update constants.ts and helpers.ts
    - Add SPEEDS constant for selecting pathfinding speed
    - Add helper functions for checking equality, sleeping, and generating random integers
    - Remove unused code and update variable names

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -39,10 +39,41 @@ export const createNewGrid = (grid: GridType, row: number, col: number) => {
     ...newGrid[row][col],
     isWall: !newGrid[row][col].isWall,
   };
+
   newGrid[row][col] = newTile;
   return newGrid;
 };
 
 export const isEqual = (a: TileType, b: TileType) => {
   return a.row === b.row && a.col === b.col;
+};
+
+export const isRowColEqual = (row: number, col: number, tile: TileType) => {
+  return row === tile.row && col === tile.col;
+};
+
+export const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+export const getRandInt = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+export const checkStack = (tile: TileType, stack: TileType[]) => {
+  for (let i = 0; i < stack.length; i++) {
+    if (isEqual(stack[i], tile)) return true;
+  }
+  return false;
+};
+
+export const dropFromQueue = (tile: TileType, queue: TileType[]) => {
+  for (let i = 0; i < queue.length; i++) {
+    if (isEqual(tile, queue[i])) {
+      queue.splice(i, 1);
+      break;
+    }
+  }
 };
